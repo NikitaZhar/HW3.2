d4b1db6b64204ecea40d9ed23a5f9b146566b34f
Nikita Zharinov <nikita.zharinov@gmail.com>
2021-03-29 16:56:54 +0200

    Initial Commit

---
+++ ColorSliders/ColorSlidersApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorSlidersApp.swift
+//  ColorSliders
+//
+//  Created by Nikita Zharinov on 29/03/2021.
+//
+
+import SwiftUI
+
+@main
+struct ColorSlidersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
