a1fc27384ab4d299565e5daea09f8062997f6050
Nikita Zharinov <nikita.zharinov@gmail.com>
2021-03-30 14:51:20 +0200

    All interface controls

--- ColorSliders/ContentView.swift
+++ ColorSliders/ContentView.swift
@@ -12,7 +12,7 @@ struct ContentView: View {
     @State private var redSliderValue = Double.random(in: 0...255)
     @State private var greenSliderValue = Double.random(in: 0...255)
     @State private var blueSliderValue = Double.random(in: 0...255)
-
+    
     var body: some View {
         ZStack {
             Color(red: 44 / 255, green: 108 / 255, blue: 188 / 255)
@@ -25,30 +25,45 @@ struct ContentView: View {
                 ))
                 VStack {
                     HStack {
-                        Text(sliderValue(from: redSliderValue))
+                        Text("\(lround(redSliderValue))")
                             .foregroundColor(.white)
                             .font(.system(size: 20))
-                            .padding(.horizontal)
-                        Slider(value: $redSliderValue, in: 0...255)
+                            .frame(minWidth: 10, maxWidth: 40, minHeight: 20, maxHeight: 20)
+                        Slider(value: $redSliderValue, in: 0...255, step: 1)
                             .accentColor(.red)
+                            .frame(height: 50)
+                        TextField("000", value: $redSliderValue, formatter: NumberFormatter())
+                            .font(.system(size: 20))
+                            .frame(width: 51, height: 35)
+                            .textFieldStyle(RoundedBorderTextFieldStyle())
                     }
                     HStack {
-                        Text(sliderValue(from: greenSliderValue))
+                        Text("\(lround(greenSliderValue))")
                             .foregroundColor(.white)
                             .font(.system(size: 20))
-                            .padding(.horizontal)
+                            .frame(minWidth: 10, maxWidth: 40, minHeight: 20, maxHeight: 20)
                         Slider(value: $greenSliderValue, in: 0...255)
                             .accentColor(.green)
+                            .frame(height: 50)
+                        TextField("000", value: $greenSliderValue, formatter: NumberFormatter())
+                            .font(.system(size: 20))
+                            .frame(width: 51, height: 35)
+                            .textFieldStyle(RoundedBorderTextFieldStyle())
                     }
                     HStack {
-                        Text(sliderValue(from: blueSliderValue))
+                        Text("\(lround(blueSliderValue))")
                             .foregroundColor(.white)
                             .font(.system(size: 20))
-                            .padding(.horizontal)
+                            .frame(minWidth: 10, maxWidth: 40, minHeight: 20, maxHeight: 20)
                         Slider(value: $blueSliderValue, in: 0...255)
-                            .accentColor(.blue)
+                            .accentColor(.green)
+                            .frame(height: 50)
+                        TextField("000", value: $blueSliderValue, formatter: NumberFormatter())
+                            .font(.system(size: 20))
+                            .frame(width: 51, height: 35)
+                            .textFieldStyle(RoundedBorderTextFieldStyle())
                     }
-                }
+                }.padding()
                 Spacer()
             }
         }
@@ -61,6 +76,3 @@ struct ContentView_Previews: PreviewProvider {
     }
 }
 
-private func sliderValue(from value: Double) -> String {
-    String(format: "%.3d", Int(value))
-}
